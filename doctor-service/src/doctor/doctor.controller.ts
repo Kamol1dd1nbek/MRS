6ad05aae4e414b34b3c109e3e05b68dfd9569d0a
Kamoliddinbek <kamoliddinbekxudoyberdi@gmail.com>
2025-07-26 22:23:40 +0500

    Doctors CRUD completed

--- doctor-service/src/doctor/doctor.controller.ts
+++ doctor-service/src/doctor/doctor.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { DoctorService } from './doctor.service';
+import { CreateDoctorDto } from './dto/create-doctor.dto';
+import { UpdateDoctorDto } from './dto/update-doctor.dto';
 
 @Controller('doctors')
 export class DoctorController {
@@ -8,5 +20,28 @@ export class DoctorController {
   @Get()
   findAll() {
     return this.doctorService.findAll();
+  }
+
+  @Get(':id')
+  getOneById(@Param('id', ParseIntPipe) id: number) {
+    return this.doctorService.findOneById(id);
+  }
+
+  @Post()
+  create(@Body(ValidationPipe) createDoctorDto: CreateDoctorDto) {
+    return this.doctorService.create(createDoctorDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateDoctorDto: UpdateDoctorDto,
+  ) {
+    return this.doctorService.update(id, updateDoctorDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.doctorService.remove(id);
   }
 }
